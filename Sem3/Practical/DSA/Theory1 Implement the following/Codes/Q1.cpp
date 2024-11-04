c52eac3430b5e12abf38f3d7eca297aba612be85
gautamkaran <karankumargautam.dev@gmail.com>
2024-11-04 11:17:32 +0530

    typo in DSA  1st practical

--- Sem3/Practical/DSA/Theory1 Implement the following/Codes/Q1.cpp
+++ Sem3/Practical/DSA/Theory1 Implement the following/Codes/Q1.cpp
@@ -5,20 +5,18 @@
 
 #include <iostream>
 using namespace std;
-int array[5] = {3, 4, 1, 7, 5};
-int size = 5;
 
-void ShowAllElementInArray()
+void ShowAllElementInArray(int array[], int size)
 {
-  for (int i = 0; i < 5; i++)
+  for (int i = 0; i < size; i++)
   {
     cout << array[i] << " ";
   }
 }
 
-void Searching(int target)
+void Searching(int array[], int size, int target)
 {
-  for (int i = 0; i < 5; i++)
+  for (int i = 0; i < size; i++)
   {
     if (target == array[i])
     {
@@ -29,9 +27,9 @@ void Searching(int target)
   cout << target << " Element is not found!";
 }
 
-int Reversing()
+int Reversing(int array[], int size)
 {
-  int start = 0, end = 4;
+  int start = 0, end = size - 1;
   while (start < end)
   {
     swap(array[start], array[end]);
@@ -41,7 +39,7 @@ int Reversing()
   cout << "Array is Revered" << endl;
 }
 
-int Sorting()
+int Sorting(int array[], int size)
 {
   for (int i = 0; i < size - 1; i++)
   {
@@ -60,6 +58,8 @@ int main()
 {
   cout << "\nWelcome to 1D Array Operation Program.\n";
   int choice;
+  int array[5] = {3, 4, 1, 7, 5};
+  int size = 5;
   do
   {
     cout << "\n1. Show All Element\n2. Searching.\n3. Sorting\n4. Reversing\n5. Exit";
@@ -71,22 +71,22 @@ int main()
     {
     case 1:
       cout << "Show All Element" << endl;
-      ShowAllElementInArray();
+      ShowAllElementInArray(array, size);
       break;
     case 2:
       cout << "Searching..." << endl;
       int element;
       cout << "Enter element to find: ";
       cin >> element;
-      Searching(element);
+      Searching(array, size, element);
       break;
     case 3:
       cout << "Sorting.." << endl;
-      Sorting();
+      Sorting(array, size);
       break;
     case 4:
       cout << "Reversing.." << endl;
-      Reversing();
+      Reversing(array, size);
       break;
     case 5:
       cout << "Program exit successfully!";
